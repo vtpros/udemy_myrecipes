610a60988e6a905c46d1439f9a7e29998593c0d5
Vladimir Trachenko <trachenko@gmail.com>
2022-10-21 11:21:45 +0300

    Fix bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,16 @@
 // that code so it'll be compiled.
 
 import 'bootstrap'
+import '../stylesheets/application'
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="popover"]').popover()
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- test/integration/pages_test.rb
+++ test/integration/pages_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PagesTest < ActionDispatch::IntegrationTest
   test 'should get home' do
-    get pages_home_url
+    get pages_home_url # /pages/home
     assert_response :success
   end
 
